9fa4e301102f16f3cc163440180785f7464162f3
Mishmore <michellemore28@gmail.com>
2018-03-23 22:49:45 -0500

    config folders and sass

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-//Install express server
 const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
+const path = require('path');
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
